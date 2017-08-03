cd91bcc6dab3aaed91d71b873dde851fd83a2b7e
Joe Romero <joethemovie@gmail.com>
2017-08-03 14:38:11 -0400

    Initial commit

---
+++ js/master.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+
+  const expiration =  '8/31/17';
+  const vehicleArray = [];
+
+  function Vehicle(
+     year,
+     make,
+     model,
+     color,
+     doors,
+     engine,
+     transmission,
+     brakes,
+     steering,
+     power,
+     equipment,
+     vin,
+     stock,
+     miles,
+     msrp,
+     leasePrice,
+     residual,
+     security,
+     bankFee,
+     moneyDown,
+     totalPayment,
+     months,
+     milesPerYear,
+     thereAfter,
+     totalAtInception,
+     additionalInfo,
+     vehicleImage,
+     vehicleUrl,
+     buyItNow
+    ){
+      this.year = year;
+      this.make = make;
+      this.model = model;
+      this.color = color;
+      this.doors = doors;
+      this.engine = engine;
+      this.transmission = transmission;
+      this.brakes = brakes;
+      this.steering = steering;
+      this.power = power;
+      this.equipment = equipment;
+      this.vin = vin;
+      this.stock = stock;
+      this.miles = miles;
+      this.msrp = msrp;
+      this.leasePrice = leasePrice;
+      this.residual = residual;
+      this.security = security;
+      this.bankFee = bankFee;
+      this.moneyDown = moneyDown;
+      this.totalPayment = totalPayment;
+      this.months = months;
+      this.milesPerYear = milesPerYear;
+      this.thereAfter = thereAfter;
+      this.totalAtInception = totalAtInception;
+      this.additionalInfo = additionalInfo;
+      this.vehicleImage = vehicleImage;
+      this.vehicleUrl = vehicleUrl;
+      this.buyItNow = buyItNow;
+  }
+
+  function listing(thisCar){
+    document.body.innerHTML += '<div class="specialContainer"><div class="specialContent leftContent"><h3 class="yearMake">' + thisCar.year + ' ' + thisCar.make + '</h3><h2 class="modelTrim">' + thisCar.model + '</h2><img src="' + thisCar.vehicleImage + '" alt="alfa vehicle image" class="vehicleImage"></div><div class="specialContent rightContent"><h3 class="leaseHead">Lease this vehicle for:</h3><div class="leaseQuoteSection"><h1 class="lease">$' + thisCar.leasePrice + '</h1><h3 class="monthsSections">/MO<br>' + thisCar.months + ' MOS</h3></div><h4 class="moneyDue">$' + thisCar.totalAtInception + ' DUE AT SIGNING</h4>' + thisCar.buyItNow + '<a href="' + thisCar.vehicleUrl + '" class="linkButton" target="_blank">VIEW VEHICLE</a><p class="disclaimer">VIN ' + thisCar.vin + '. Stk ' + thisCar.stock + '. ' + thisCar.doors + ', ' + thisCar.engine + ', ' + thisCar.transmission + ', air conditioning, power steering/ABS brakes/windows/locks/mirrors, cd/MP3, ' + thisCar.equipment + '. MSRP: $' + thisCar.msrp + '. $' + thisCar.totalAtInception + ' due at inception. $' + thisCar.moneyDown + ' down + 1st mo pymt $' + thisCar.leasePrice + '+ bank fee of $595 incl. + $0 sec dep. $' + thisCar.totalPayment + ' ttl pymts/$' + thisCar.residual + ' residual.' + thisCar.additionalInfo + ' Prices include all costs to be paid by a consumer except for licensing, registration, taxes, and $299 doc. fee. Tier 1 credit approval thru primary lender. Lessee resp. for excess wear/tear. 10K mi. per yr/.25¢ per mi. over thereafter. See dealer for complete details. Not resp. for typos. Offers expire ' + expiration + '.</p></div></div>';
+
+  }
+
+
+// <div style="margin-bottom: 20px;background-color:#fff;">Buy Now Price: <span style="font-weight: 900; font-size: 35px;background-color: #fff;">$25,995<sup>*</sup></span></div>
+
+  for (var i = 0; i < vehicleArray.length; i++) {
+    listing(vehicleArray[i]);
+  }
+
+
+
+});
